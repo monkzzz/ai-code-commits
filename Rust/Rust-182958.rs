7e758af02364dbf47a7c80e784a7bb187c9e4268
monkzzz <fabsalgadinho@gmail.com>
2024-06-16 18:29:58 +0100

    Add Rust-18:29:58.rs

---
+++ Rust/Rust-182958.rs
@@ -0,0 +1,7 @@
+ Sure, here's some sample code in Rust that prints "Hello World" to the console:
+
+```rust
+println!("Hello world");
+```
+
+This code uses the `println()` function from the `std::io` module to print the string "Hello world" to the console. You can replace the text inside the `println()` call with any other message you want to display.
