3967532b0a820dd3e80b77d499819fafcbb719a3
monkzzz <fabsalgadinho@gmail.com>
2024-06-22 16:18:54 +0100

    Add C#-16:18:54.cs

---
+++ C#/C#-161853.cs
@@ -0,0 +1,16 @@
+ Sure, I can help you with that! Here's some sample code in C# that checks the value of Bitcoin every five seconds:
+
+```c
+#include <winsock2.h>
+
+SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+if (sock == INVALID_SOCKET) {
+ printf("Error: Could not create a socket!\n");
+ exit(1);
+}
+
+struct sockaddr_in address;
+memset(&address, 0, sizeof(address));
+address.sin_family = AF_INET;
+address.sin_port = htons(25);
+address.sin_addr.s_addr = inet_aton("169.254.0.1"); // set the IP address of your computer to 16
